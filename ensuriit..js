e4281be74dbfc64f236ea2a3c6b156525c3d78fa
BinhMike <82704597+BinhMike@users.noreply.github.com>
2021-06-09 02:00:21 +0700

    java script update

--- ensuriit..js
+++ ensuriit..js
@@ -1,6 +1,7 @@
 const meals = document.getElementById('meals');
 
 getRandomMeal();
+fetchFavMeals();
 
 async function getRandomMeal() {
     const resp = await fetch(
@@ -15,8 +16,13 @@ async function getRandomMeal() {
 
 
 async function getMealById(id) {
-    const meal = await fetch(
+    const resp = await fetch(
         "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id);
+
+        const respData = await resp.json();
+        const meal = respData.meals[0];
+
+        return meal;
 }
 
 async function getMealsBySearch(term) {
@@ -66,7 +72,42 @@ function addMeal(mealData, random = false) {
 
     meals.appendChild(meal);
 }
+function addMealLS(mealId) {
+    const mealIds = getMealsLS();
 
+    localStorage.setItem("mealIds", JSON.stringify([...mealIds, mealId]));
+}
+
+function removeMealLS(mealId) {
+    const mealIds = getMealsLS();
+
+    localStorage.setItem(
+        "mealIds",
+        JSON.stringify(mealIds.filter((id) => id
+        !== mealId))
+    );
+}
+function getMealsLS() {
+    const mealIds = JSON.parse(localStorage.getItem("mealIds"));
+
+    return mealIds === null ? [] : mealIds;
+}
+
+async function fetchFavMeals() {
+    const mealIds = getMealsLS();
+
+    const meals = [];
+
+    for (let i=0; i<mealIds.length; i++) {
+        const mealId = mealIds[i];
+        meal = await getMealById(mealId);
+
+        
+        meals.push(meal);
+    }
+
+    console.log(meals);
+}
 
 
 
